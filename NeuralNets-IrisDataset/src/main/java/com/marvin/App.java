5d247364a61b0181323c209a761266e75e4b3fa4
Marvin Bertin <marvin.bertin@gmail.com>
2016-01-31 15:29:14 -0800

    Add Iris NN folder

---
+++ NeuralNets-IrisDataset/src/main/java/com/marvin/App.java
@@ -0,0 +1,13 @@
+package com.marvin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
